85ca826ab2dcd63a39075e81882c1ff2af509e4d
Err1ck <erryck-1996@hotmail.com>
2023-08-01 13:18:06 +0200

    Ej 1-3

--- jsv-1/exercise.js
+++ jsv-1/exercise.js
@@ -1,4 +1,20 @@
+/*
+# Classes
+
+Define a class called `Person` that takes in two arguments (`firstName` and `lastName`) in the constructor.
+
+const developer = new Person('Mario', 'Rossi');
+console.log(developer.firstName + " " + developer.lastName);
+*/
+
 // Class definition
+class Person{
+    constructor(firstName,lastName){
+        this.firstName=firstName;
+        this.lastName=lastName;
+    }
+}
+
 
 const developer = new Person('Mario', 'Rossi');
 console.log(developer.firstName + " " + developer.lastName);
--- jsv-4/exercise.js
+++ jsv-4/exercise.js
@@ -1,5 +1,12 @@
+/*
+# Classes
+
+Define a class called `Person` that takes in three parameters in the constructor 
+(`firstName`, `lastName` and `age`). 
+Create the getters and setters methods for each property and a getter method called `fullName` that returns the full name.
+
 class Person {
-  // ...
+// ...
 }
 
 const person = new Person('Mario', 'Rossi', 25);
@@ -7,4 +14,24 @@ console.log(person.fullName);
 
 person.firstName = 'Maria';
 person.lastName = 'Verdi';
+console.log(person.fullName);
+*/
+
+class Person {
+  constructor(firstName,lastName,age){
+    this._firstName=firstName;
+    this._lastName=lastName;
+    this._age=age;
+  }
+  get fullName(){
+    return this._firstName +" "+ this._lastName
+  }
+  
+}
+
+const person = new Person('Mario', 'Rossi', 25);
+console.log(person.fullName);
+
+person._firstName = 'Maria';
+person._lastName = 'Verdi';
 console.log(person.fullName);
